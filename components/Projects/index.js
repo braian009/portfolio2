808dfdcb5ba02ea5389ae08df8b6eecf74927f28
braian <braianyanquin@gmail.com>
2023-05-01 23:22:30 -0300

    projects section's layout mockup added

---
+++ components/Projects/index.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import styles from "./projects.module.css";
+
+const Projects = () => {
+  
+  return (
+      <div className={styles.container}>
+     
+        <div className={styles.inner}>
+          <h1>Projects</h1>
+          <div className={styles.projectsContainer}>
+            <div style={{height: '100%', width: '100%'}}>
+            </div>
+            <div style={{height: '100%', width: '100%'}}>
+            </div>
+            <div style={{height: '100%', width: '100%'}}>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Projects;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 
 export default function Home() {
@@ -19,6 +20,7 @@ export default function Home() {
       <Hero/>
       <About/>
       <Skills/>
+      <Projects/>
       </>
     </>
   );
