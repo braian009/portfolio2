e7197fdef396c3dee66de161dcaab027b4f05a51
braian <braianyanquin@gmail.com>
2023-05-15 23:49:42 -0300

    -

--- components/Hero/ProfilePic/index.js
+++ components/Hero/ProfilePic/index.js
@@ -1,30 +1,30 @@
-import * as React from 'react'
+import * as React from "react";
 
-import Image from 'next/image';
-import { motion } from 'framer-motion';
+import Image from "next/image";
+import { motion } from "framer-motion";
 
-import styles from './ProfilePic.module.css';
+import styles from "./profilePic.module.css";
 
 const ProfilePic = () => {
   return (
     <motion.div
-          className={styles.picture}
-          initial={{ x: -100, opacity: 0 }}
-          whileInView={{
-            opacity: 1,
-            x: 0,
-            transition: {
-              duration: 0.4,
-              type: "spring",
-              stiffness: 400,
-              damping: 75,
-            },
-          }}
-          viewport={{ once: "true" }}
-        >
-          <Image src="/assets/hero/profilepic.jpg" alt={""} fill />
-        </motion.div>
-  )
-}
+      className={styles.picture}
+      initial={{ x: -100, opacity: 0 }}
+      whileInView={{
+        opacity: 1,
+        x: 0,
+        transition: {
+          duration: 0.4,
+          type: "spring",
+          stiffness: 400,
+          damping: 75,
+        },
+      }}
+      viewport={{ once: "true" }}
+    >
+      <Image src="/assets/hero/profilepic.jpg" alt={""} fill />
+    </motion.div>
+  );
+};
 
 export default ProfilePic;
